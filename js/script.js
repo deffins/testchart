0930b7c8755cd295270c8803bc9a40489ffddb95
deffins <dainis@fons.lv>
2022-10-15 19:03:41 +0300

    a

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function onSVGLoaded(data) {
     // });
     // Snap.getElementByPoint(mouseX, mouseY).attr({ stroke: "#f00" });
 
-    s.data.onclick = function (data) {
+    s.node.onclick = function (data) {
         console.log(Snap(this));
     }
 
