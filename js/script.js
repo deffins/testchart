f066e867c5b658af304ae1cf29d588515c8173b8
deffins <dainis@fons.lv>
2022-10-15 17:48:28 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ function onSVGLoaded(data) {
     let rect = Snap(s.select('#chronic-thc'));
     // let rect2 = Snap(s.select('#BIeTZq3KbKyFNtU_o8Uz-20'));
     console.log(rect);
-    rect.selectAll('g');
-    // console.log(rect2);
+    let groups = rect.selectAll('g');
+    console.log(groups);
 
 
     // rect.attr({
