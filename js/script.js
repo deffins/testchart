9853ae4880e8630c71392c4e3196954f423f6b33
deffins <dainis@fons.lv>
2022-10-23 16:38:56 +0300

    learning snap n shit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,60 @@
 
 var s = Snap();
-Snap.load("../chronic-thc-chart.svg", onSVGLoaded);
+Snap.load("../dm.svg", onSVGLoaded);
 
 function onSVGLoaded(data) {
     s.append(data);
     // console.log(data);
     // console.log(s);
 
-    let rect = Snap(s.select('#chronic-thc'));
-    // let rect2 = Snap(s.select('#BIeTZq3KbKyFNtU_o8Uz-20'));
-    console.log(rect);
-    let groups = rect.selectAll('path');
-    console.log(groups);
+    // let rect = Snap(s.select('#chronic-thc'));
+    // // let rect2 = Snap(s.select('#BIeTZq3KbKyFNtU_o8Uz-20'));
+    // console.log(rect);
+    // let groups = rect.selectAll('path');
+    // console.log(groups);
+
+    // let rectArr = Snap(s.selectAll('rect'));
+    // let rectArr2 = Snap(s.selectAll('#g'));
+    // // let rect2 = Snap(s.select('#BIeTZq3KbKyFNtU_o8Uz-20'));
+    // console.log(rectArr);
+    // console.log(rectArr2);
 
 
-    // rect.attr({
-    //     stroke: "#263a1b",
-    //     strokeWidth: 6,
-    // });
-    // Snap.getElementByPoint(mouseX, mouseY).attr({ stroke: "#f00" });
+    Snap.selectAll("rect").forEach(function (element) {
 
-    s.node.onclick = function (data) {
-        console.log(Snap(this));
+        element.click(function (event) {
+            console.log(Snap(event.srcElement).parent().attr().id)
+            // console.log(arguments)
+            // console.log(event)
+            if (!event.srcElement.classList.contains("selected")) {
+                Snap(event.srcElement).attr("fill", "lightblue");
+                Snap(event.srcElement).attr("strokeWidth", "3");
+                Snap(event.srcElement).addClass("selected");
+            } else {
+                Snap(event.srcElement).attr("fill", "none");
+                Snap(event.srcElement).attr("strokeWidth", "1");
+                Snap(event.srcElement).removeClass("selected");
+            }
+        });
+    });
+
+    let rects = document.getElementsByTagName("rect");
+    for (let i = 0; i < rects.length; i++) {
+        let rect = rects[i];
+        let pointerEvent = rect.getAttribute("pointer-events");
+        if (pointerEvent == "none") {
+            rect.setAttribute("pointer-events", "all");
+        }
+    }
+    let divs = document.getElementsByTagName("div");
+    for (let i = 0; i < divs.length; i++) {
+        let div = divs[i];
+        let pointerEventValue = div.style.getPropertyValue("pointer-events");
+        if (pointerEventValue == "all") {
+            div.style.setProperty("pointer-events", "none");
+        }
     }
 
-    let rectArr = Snap(s.selectAll('rect'));
-    let rectArr2 = Snap(s.selectAll('#g'));
-    // let rect2 = Snap(s.select('#BIeTZq3KbKyFNtU_o8Uz-20'));
-    console.log(rectArr);
-    console.log(rectArr2);
 
 }
 
