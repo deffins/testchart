136754ac39144a64256b9bf712a2f6c3bd3328cd
deffins <dainis@fons.lv>
2023-02-06 11:09:44 +0200

    cors

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+//import cors library from cors npm package
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+
+app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+})
+
+
+//configure cors to allow cross origin requests
+
+
+
 // import thcData from "data/thc-info.json"
 const myRequest = new Request('data/thc-info.json');
 this.links = [];
